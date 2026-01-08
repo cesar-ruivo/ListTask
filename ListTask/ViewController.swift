afeef362a0be55bee9c00caf7217f818cfa8b367
Cesar Ruivo <cesar-ruivo>
2026-01-08 16:52:06 -0300

    Initial Commit

---
+++ ListTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ListTask
+//
+//  Created by Mag on 08/01/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
